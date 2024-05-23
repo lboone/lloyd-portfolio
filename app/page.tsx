cb18a14a7112a66f6e65f4161325647df35dce12
Lloyd Boone <lboone@progressive.com>
2024-05-23 12:51:15 -0400

    implement hero and added public assets.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
+interface NavItemProps {
+  name: string;
+  link: string;
+  icon?: JSX.Element;
+}
+const navItems: NavItemProps[] = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <FaHome />,
+  },
+];
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
       </div>
     </main>
